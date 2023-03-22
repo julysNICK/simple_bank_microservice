ed831a6d41481d2f3efd23a688d8987f9dc74858
julysNICK <julystube@gmail.com>
2023-03-22 18:39:22 -0300

    insert entry tests

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -1,1 +1,65 @@
 package db
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func CreateRandomEntry(t *testing.T) Entry {
+	args := CreateEntryParams{
+		AccountID: sql.NullInt64{Int64: 24, Valid: true},
+		Amount:    1000,
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), args)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, args.AccountID.Int64, entry.AccountID.Int64)
+	require.Equal(t, args.Amount, entry.Amount)
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	CreateRandomEntry(t)
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := CreateRandomEntry(t)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestListEntries(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		CreateRandomEntry(t)
+	}
+
+	arg := ListEntriesParams{
+		AccountID: sql.NullInt64{Int64: 24, Valid: true},
+		Limit:     5,
+		Offset:    5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
+}
