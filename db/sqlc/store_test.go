806b56cb8509746d5e0819ad07d8154e1f0dfc1b
julysNICK <julystube@gmail.com>
2023-03-31 17:38:32 -0300

    rename migrate

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -89,7 +89,7 @@ func TestTransferTx(t *testing.T) {
 
 		diff1 := account1.Balance - fromAccount.Balance
 		diff2 := toAccount.Balance - account2.Balance
-		fmt.Println(">> diff", diff1, diff2)
+		fmt.Println(">> dif2f", diff1, diff2)
 		require.Equal(t, diff1, diff2)
 		require.True(t, diff1 > 0)
 		require.True(t, diff1%amount == 0)
