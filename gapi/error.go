b6862f407b288ed7e49deb74468c7cfc469f2de1
julysNICK <julystube@gmail.com>
2023-05-10 14:56:45 -0300

    add validator argument in grpc

---
+++ gapi/error.go
@@ -0,0 +1,29 @@
+package gapi
+
+import (
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func fieldViolation(field string, description string) *errdetails.BadRequest_FieldViolation {
+	return &errdetails.BadRequest_FieldViolation{
+		Field:       field,
+		Description: description,
+	}
+}
+
+func invalidArgumentError(violations []*errdetails.BadRequest_FieldViolation) error {
+			badRequest := &errdetails.BadRequest{
+			FieldViolations: violations,
+		}
+		statusInvalid := status.New(codes.InvalidArgument, "invalid argument")
+
+		statusInvalid, err := statusInvalid.WithDetails(badRequest)
+
+		if err != nil {
+			return  err
+		}
+
+		return  statusInvalid.Err()
+}
